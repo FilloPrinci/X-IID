e18207c10c21436eda2103740d2234781fb47700
FilloPrinci <filloprinci98@gmail.com>
2022-07-24 15:24:33 +0200

    Feat: stick to ground

--- X-IID/Assets/Scripts/Player/CarController.cs
+++ X-IID/Assets/Scripts/Player/CarController.cs
@@ -11,16 +11,25 @@ public class CarController : MonoBehaviour
     [Range(0f, 10f)]
     public float turnStrenght;
 
+    public Transform frontCollisionCheck;
+    public Rigidbody rb;
+    public float groundOffset;
+
+
     private float inputAccellerator = 0;
     private float inputBrake = 0;
     private float inputTourn = 0;
     private float speed = 0;
     private Vector3 previousPosition;
     private float currentAccelleration = 0;
+    private Vector3 accellerationVector;
+    private Vector3 gravityVector;
+    private Vector3 groundRotation;
 
     // Start is called before the first frame update
     void Start()
     {
+        speed = 0;
         previousPosition = transform.position;
     }
 
@@ -32,17 +41,49 @@ public class CarController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        stickToGound();
         getInputs();
         accellerate();
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, inputTourn * turnStrenght * 100 * Time.deltaTime, 0f));
+        transform.position += gravityVector + accellerationVector;
+        
     }
 
     void accellerate() {
         currentAccelleration = Mathf.Lerp(speed, (maxSpeeed * inputAccellerator * 10), Time.deltaTime * accelleration);
-        transform.position += transform.forward * currentAccelleration * Time.deltaTime;
-        
-        speed = (transform.position - previousPosition).magnitude / Time.deltaTime;
+        accellerationVector = transform.forward * currentAccelleration * Time.deltaTime;
+        Debug.Log("speed: " + speed + " | accelleration: " + currentAccelleration + " | InputAccelleration: " + inputAccellerator);
+        speed = transform.InverseTransformDirection(transform.position - previousPosition).z /Time.deltaTime;
         previousPosition = transform.position;
+    }
+
+    void stickToGound() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, -transform.up * hit.distance, Color.yellow);
+            gravityVector = -transform.up * (hit.distance - groundOffset);
+            //gravityVector = Vector3.LerpUnclamped(gravityVector, -transform.up * (hit.distance - groundOffset), Time.deltaTime);
+            
+            groundRotation = Quaternion.FromToRotation(hit.normal, Vector3.up).eulerAngles;
+        }
+        else {
+            gravityVector = Vector3.zero;
+        }
+    }
+
+    void physicsAccellerate() {
+        currentAccelleration = Mathf.Lerp(speed, (maxSpeeed * inputAccellerator /2), accelleration);
+
+        rb.AddForce(transform.forward * currentAccelleration, ForceMode.VelocityChange);
+        Debug.Log("force vector: " + (transform.forward * currentAccelleration) + "speed: " + speed );
+
+        speed = rb.velocity.z;
+        previousPosition = transform.position;
+    }
+
+    void physicsRotate() {
+        rb.AddTorque(transform.up * inputTourn * turnStrenght * 100, ForceMode.Force);
     }
 
     void getInputs() {
@@ -50,4 +91,8 @@ public class CarController : MonoBehaviour
         inputBrake = Input.GetAxis("Brake");
         inputTourn = Input.GetAxis("Horizontal");
     }
+
+    void checkFastCollision() { 
+        
+    } 
 }
